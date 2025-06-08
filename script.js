fc2fd7af64fba8410618ea4fa91ae8ad11d9abf5
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-06-08 22:28:18 +1200

    Create the function getHumanChoice()

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let userInput = prompt("Type 1 - 3 to choose your player. 1 - Rock, 2 - Paper, 3 - Scissors");
+    if (userInput == 1) {
+        return "rock";
+    } else if (userInput == 2) {
+        return "paper";
+    } else if (userInput == 3) {
+        return "scissors";
+    } else {
+        return "you didn't type the available options";
+    }
+}
+
+console.log(getHumanChoice());
