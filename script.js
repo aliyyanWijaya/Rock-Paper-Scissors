67ae4792d2a99c252168e49b0740f95f9c96e109
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-06-08 22:10:12 +1200

    Create the function getComputerChoice to return random rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+function getComputerChoice() {
+    const getRandomNum = Math.floor(Math.random() * 10);
+    if (getRandomNum < 2) {
+        return "rock";
+    } else if(getRandomNum < 6) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
